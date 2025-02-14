c8f752cd3de12fb430d54eb7a6ec0b70ee8d7d68
K-G8002 <kgrawitter@gmail.com>
2025-02-14 15:29:00 +0100

    Profile SaveButton deleted

--- src/main/java/org/vaadin/example/Website.java
+++ src/main/java/org/vaadin/example/Website.java
@@ -1,6 +1,5 @@
 package org.vaadin.example;
 
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Image;
@@ -57,23 +56,12 @@ public class Website extends VerticalLayout {
                 currentAvatarIndex = (currentAvatarIndex + 1) % avatarUrls.size();
                 avatarImage.setSrc(avatarUrls.get(currentAvatarIndex));
             });
+            add(new HorizontalLayout(previousButton, avatarImage, nextButton));
 
             TextField nicknameField = new TextField("Nickname");
             nicknameField.setPlaceholder("LustigerName");
+            add(nicknameField);
 
-            Button saveButton = new Button("Speichern", event -> {
-                String selectedNickname = nicknameField.getValue();
-                if (selectedNickname.isEmpty()) {
-                    Notification.show("Bitte gib einen Nickname ein.");
-                    return;
-                }
-                session.setAttribute("nickname", selectedNickname);
-                session.setAttribute("avatar", avatarUrls.get(currentAvatarIndex));
-                UI.getCurrent().getPage().reload();
-            });
-
-            add(new HorizontalLayout(previousButton, avatarImage, nextButton));
-            add(nicknameField, saveButton);
         } else {
             Image avatarImage = new Image(avatarUrl, "Avatar");
             avatarImage.setWidth("100px");
@@ -122,135 +110,6 @@ public class Website extends VerticalLayout {
 
 
 
-
-/*@Route("login")
-class login extends VerticalLayout {
-
-    public login() {
-        addClassName("centered-content");
-        getStyle().set("background-color", "var(--lumo-contrast-5pct)")
-                .set("display", "flex")
-                .set("justify-content", "center")
-                .set("padding", "var(--lumo-space-l)");
-
-        LoginI18n i18n = LoginI18n.createDefault();
-        LoginI18n.Form i18nForm = i18n.getForm();
-        i18nForm.setTitle("Login");
-        i18nForm.setUsername("Username");
-        i18nForm.setPassword("Password");
-        i18nForm.setSubmit("Submit");
-        i18nForm.setForgotPassword("Unknown Password");
-        i18n.setForm(i18nForm);
-        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
-        i18nErrorMessage.setTitle("Problem");
-        i18nErrorMessage.setMessage("Username or Password is false");
-        i18n.setErrorMessage(i18nErrorMessage);
-
-        LoginForm loginForm = new LoginForm();
-        loginForm.setI18n(createLoginI18n());
-
-        loginForm.addLoginListener(event -> {
-            String username = event.getUsername();
-            String password = event.getPassword();
-
-            if ("Selbsthilfegruppe".equals(username) && GroupManager.isSelfhilfegruppeLoggedIn()) {
-                loginForm.setError(true);
-                com.vaadin.flow.component.notification.Notification.show("Der Account 'Selbsthilfegruppe' ist bereits angemeldet.");
-                return;
-            }
-
-            if (authenticate(username, password)) {
-                VaadinSession.getCurrent().setAttribute("user", username);
-
-                if ("Selbsthilfegruppe".equals(username)) {
-                    VaadinSession.getCurrent().setAttribute("nickname", "Admin");
-                    VaadinSession.getCurrent().setAttribute("avatar", "https://www.gravatar.com/avatar/placeholder?s=100&d=mp");
-                    GroupManager.setSelfhilfegruppeLoggedIn(true);
-                } else {
-                    VaadinSession.getCurrent().setAttribute("nickname", null);
-                    VaadinSession.getCurrent().setAttribute("avatar", null);
-                }
-                UI.getCurrent().navigate("website");
-            } else {
-                loginForm.setError(true);
-            }
-        });
-
-        loginForm.getElement().setAttribute("no-autofocus", "");
-        add(loginForm);
-
-        Button registerButton = new Button("Register", event -> UI.getCurrent().navigate("register"));
-        add(registerButton);
-    }
-
-    private boolean authenticate(String username, String password) {
-        return ("Selbsthilfegruppe".equals(username) && "lol".equals(password)) ||
-                ("User".equals(username) && "12345".equals(password));
-    }
-
-    private LoginI18n createLoginI18n() {
-        LoginI18n i18n = LoginI18n.createDefault();
-        LoginI18n.Form i18nForm = i18n.getForm();
-        i18nForm.setTitle("Login");
-        i18nForm.setUsername("Username");
-        i18nForm.setPassword("Password");
-        i18nForm.setSubmit("Submit");
-        i18nForm.setForgotPassword("Unknown Password");
-        i18n.setForm(i18nForm);
-        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
-        i18nErrorMessage.setTitle("Problem");
-        i18nErrorMessage.setMessage("Username or Password is false");
-        i18n.setErrorMessage(i18nErrorMessage);
-        return i18n;
-    }
-}
-
-
-@Route("register")
-class RegisterView extends VerticalLayout {
-
-    private static HashMap<String, String> userDatabase = new HashMap<>();
-
-    public RegisterView() {
-        addClassName("centered-content");
-        getStyle().set("background-color", "var(--lumo-contrast-5pct)")
-                .set("display", "flex")
-                .set("justify-content", "center")
-                .set("padding", "var(--lumo-space-l)");
-
-        TextField usernameField = new TextField("Username");
-        TextField passwordField = new TextField("Password");
-        Button registerButton = new Button("Register", event -> {
-            String username = usernameField.getValue();
-            String password = passwordField.getValue();
-
-            if (username.isEmpty() || password.isEmpty()) {
-                usernameField.setErrorMessage("Fields cannot be empty");
-                usernameField.setInvalid(true);
-                return;
-            }
-
-            if ("Selbsthilfegruppe".equals(username)) {
-                usernameField.setErrorMessage("Dieser Username ist reserviert.");
-                usernameField.setInvalid(true);
-                return;
-            }
-
-            if (userDatabase.containsKey(username)) {
-                usernameField.setErrorMessage("Username already exists");
-                usernameField.setInvalid(true);
-            } else {
-                userDatabase.put(username, password);
-                UI.getCurrent().navigate("login");
-            }
-        });
-        add(usernameField, passwordField, registerButton);
-    }
-
-    public static boolean validateUser(String username, String password) {
-        return userDatabase.containsKey(username) && userDatabase.get(username).equals(password);
-    }
-}*/
 
 
 
